dc40c45bfe6a7a7eb57c9d497320dbef222c6afd
DatBoi <juliq.jq@gmail.com>
2025-02-09 11:18:09 -0500

    fix: update background image URL in Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -61,7 +61,7 @@ const Home = () => {
           <div 
             className="absolute inset-0 bg-cover bg-center bg-no-repeat"
             style={{
-              backgroundImage: `url('https://pbs.twimg.com/profile_banners/1801046952143900672/1738130477/1500x500')`,
+              backgroundImage: `url('blob:https://imgur.com/d7d32447-bb38-442c-b9b0-4a499bb0e271')`,
             }}
           />
           {/* Overlay oscuro con gradiente */}
